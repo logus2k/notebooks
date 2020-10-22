95043ff2c5012d518da22e4785319f7ddb8a7bd8
logus2k <logus2k@gmail.com>
2020-10-22 11:47:56 +0100

    Adds radicals.py

---
+++ radicals.py
@@ -0,0 +1,35 @@
+""" Math helper functions
+"""
+
+from decimal import Decimal
+
+
+def cube_root(n: float) -> Decimal:
+
+    root: Decimal = Decimal(n ** (1.0 / 3.0))
+    root_rounded: Decimal = Decimal(round(root) ** 3)
+
+    if root_rounded == n:
+        root = root_rounded
+
+    return root
+
+
+def list_cube_roots(number_of_roots_to_list: int):
+
+    roots_counter: int = 0
+    current_number = 0
+
+    while roots_counter < number_of_roots_to_list:
+
+        current_number += 1
+        root = cube_root(current_number)
+
+        if root == int(root):
+            roots_counter += 1
+            print(f"{roots_counter} is a perfect cube of {root} = " +
+                  f"({roots_counter} x {roots_counter} x {roots_counter} = " +
+                  f"{roots_counter}^3)")
+
+
+list_cube_roots(500)
